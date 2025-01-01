c12022e21e62a685e55d44307d2a7177e32d1ff0
XandarHub <foxtvthai91@gmail.com>
2025-01-01 10:36:15 +0700

    Create Take.lua

---
+++ Take.lua
@@ -0,0 +1,39 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/XandarHUB3/FISCH/main/Protected_3117701181880286.lua.txt"))()
+
+local Window = Library:CreateWindow({
+    Title = '<font color="#0d48c9">Razer</font><font color="#FFFFFF"> Hub</font>',
+    Icon = 78836839305136
+    ColorWindowMain = Color3.fromRGB(0, 0, 139),
+    Keybind = Enum.KeyCode.LeftControl
+})
+
+local Page1 = Window:CreateTab({
+    Title = "General",
+    Icon = "home"
+})
+
+local Section2 = Page1:CreateSection({
+    Title = "This is a section Left",
+    Side = "Left"
+})
+
+Section2:CreateToggle({
+    Title = "This is a toggle",
+    Value = false,
+    Callback = function(value)
+        getgenv().Walk = value
+        task.spawn(function()
+            while getgenv().Walk do
+                task.wait()
+                pcall(function()
+                    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+                end)
+            end
+            if not getgenv().Walk then
+                pcall(function()
+                    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+                end)
+            end
+        end)
+    end
+})
